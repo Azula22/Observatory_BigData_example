e73ba3c098a823d7cf5fac84a1b40fb8785543d3
Azula22 <azulazulana@gmail.com>
2017-04-29 00:19:34 +0300

    Little refactor

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -6,7 +6,7 @@ import java.time.LocalDate
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
-
+import Names._
 /**
   * 1st milestone: data extraction
   */
@@ -39,34 +39,51 @@ object Extraction {
     ???
   }
 
-  private val stn = StructField("STN", LongType, nullable = false)
-  private val wban = StructField("WBAN", LongType, nullable = true)
+  private val stn = StructField(STN, LongType, nullable = false)
+  private val wban = StructField(WBAN, LongType, nullable = true)
 
   private val stationsSchema = StructType(stn :: wban ::
-      StructField("lat", DoubleType, nullable = true) ::
-      StructField("long", DoubleType, nullable = true) :: Nil
+      StructField(Latitude, DoubleType, nullable = true) ::
+      StructField(Longitude, DoubleType, nullable = true) :: Nil
   )
+
+  private def stationsRow(line: List[String]): Row = {
+    val (stnR, wbanR, latR, longR) = (line.head, line(1), line(2), line(3))
+    Row(stnR.toLong, wbanR.toLong, latR.toDouble, longR.toDouble)
+  }
 
   private val yearSchema = StructType(
     stn :: wban ::
-    StructField("month", IntegerType, nullable = false) ::
-    StructField("day", IntegerType, nullable = false) ::
-    StructField("temperature", IntegerType, nullable = false) :: Nil
+    StructField(Month, IntegerType, nullable = false) ::
+    StructField(Day, IntegerType, nullable = false) ::
+    StructField(Temperature, IntegerType, nullable = false) :: Nil
   )
 
-  private def read(resource: String, structType: StructType): DataFrame = {
+  private def yearRow(line: List[String]): Row = {
+    val (stnR, wbanR, monthR, dayR, tempR) = (line.head, line(1), line(2), line(3), line(4))
+    Row(stnR.toLong, wbanR.toLong, monthR.toInt, dayR.toInt, tempR.toInt)
+  }
+
+  private def read(resource: String, structType: StructType, rowFunc: List[String] ⇒ Row): DataFrame = {
     val rdd = spark.sparkContext.textFile(getResource(resource))
     val lines = rdd
         .map(_.split(",").to[List])
-        .map(row)
+        .map(rowFunc)
 
     spark.createDataFrame(lines, structType)
   }
 
-  private def row(lines: List[String]): Row = Row(lines: _*)
-
   private def getResource(name: String): String =
     Paths.get(getClass.getResource(name).toURI).toString
 
+}
 
+object Names {
+  val STN = "STN"
+  val WBAN = "WBAN"
+  val Latitude = "latitude"
+  val Longitude = "longitude"
+  val Month = "month"
+  val Day = "day"
+  val Temperature = "temperature"
 }
