65cf5756918aa1794290cba76bf3e531053e20bf
Azula22 <azulazulana@gmail.com>
2017-05-13 23:58:11 +0300

    interpolateColor written but not tested at all

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,17 +1,36 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.Image
+
 
 /**
   * 2nd milestone: basic visualization
   */
 object Visualization {
 
+
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
     * @param location Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
+
+  val coef = 2
+
+//  val colorsMap: Map[Int, Color] = Map(
+//    60 → Color(255, 255, 255),
+//    32 → Color(255, 0, 0),
+//    12 → Color(255, 255, 0),
+//    0 → Color(0, 255, 255),
+//    -15 → Color(0, 0, 255),
+//    -27 → Color(255, 0, 255),
+//    -50 → Color(33, 0, 107),
+//    -60 → Color(0, 0, 0)
+//  )
+
+  val TemperatureColumn = "temperature"
+  val ColorColumn = "color"
+
   def predictTemperature(temperatures: Iterable[(Location, Double)], location: Location): Double = {
     ???
   }
@@ -22,7 +41,39 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Double, Color)], value: Double): Color = {
-    ???
+
+    val pointsMap = points.toMap
+
+    pointsMap.getOrElse(value, {
+
+      val sortedKeys = pointsMap.keys.toVector.sorted
+
+      val mayBeResult = for {
+        leftKey ← sortedKeys.takeWhile(_ < value).lastOption
+        rightKey ← sortedKeys.collectFirst{ case v if v > value ⇒ v }
+      } yield {
+
+        val Color(red1, green1, blue1) = pointsMap(leftKey)
+        val Color(red2, green2, blue2) = pointsMap(rightKey)
+
+        val Length = rightKey - leftKey
+        val length = value - leftKey
+
+        if (Length != 0 && length != 0) {
+          val factor = length / Length
+          val red3 = red1 + ((red2 - red1) * factor)
+          val green3 = green1 + ((green2 - green1) * factor)
+          val blue3 = blue1 + ((blue2 - blue1) * factor )
+
+          Color(red3.toInt, green3.toInt, blue3.toInt)
+
+        } else pointsMap(0)
+      }
+
+      mayBeResult.getOrElse(pointsMap(0))
+
+    })
+
   }
 
   /**
